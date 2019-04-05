4b8a9795ccf0f68a040880f380f84137633359fa
Eric Antunes <eric.t1170.vitaminaweb@xpi.com.br>
2019-04-05 17:00:38 -0300

    Regras de filtragens

--- js.js
+++ js.js
@@ -1,13 +1,44 @@
 $(document).ready(function () {
 
-  $('#filter').slick({
+  let filterSlick = $('#filter');
+
+  filterSlick.slick({
     infinite: false,
-    slidesToShow: 4,
-    slidesToScroll: 2
+    slidesToShow: 3,
+    slidesToScroll: 3
   });
 
-  $('#filter').slick('slickFilter', function(){
-    //return jQuery':even';
+  $('input[name="dia"]').change(function() {
+    let d = $(this).val();
+    let c = $('#op-categoria').val();
+    filtra( d, c );
   });
+
+  $('#op-categoria').change(function() {
+    let d = $('input[name="dia"]:checked').val();
+    let c = $(this).val();
+    filtra( d, c );
+  });
+
+  function filtra( d, c ) {
+    filterSlick.slick('slickUnfilter');
+    filterSlick.slick( 'slickFilter', function() {
+
+      let retorno;
+
+      if ( d === 'all' && c !== 'all' ) {
+        retorno = ( $('input[name="dia"]').val() === d ) && $(this).hasClass( c );
+      } else if ( d !== 'all' && c === 'all' ) {
+        retorno = ( $(this).data('dia') === parseInt( d ) ) && $('#op-categoria').val() === c;
+      } else if ( d !== 'all' && c !== 'all' ) {
+        retorno = ( $(this).data('dia') === parseInt( d ) ) && $(this).hasClass( c );
+      } else {
+        retorno = true;
+      }
+
+      return retorno;
+    } );
+  }
 
 });
+
